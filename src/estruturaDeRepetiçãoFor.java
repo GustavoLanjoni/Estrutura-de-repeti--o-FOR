dce175b7f106ff8711b59f7cee887591ff45dd32
Gustavo <gustavosilva94514@gmail.com>
2024-02-27 23:19:13 -0300

    Aula 07

---
+++ src/estruturaDeRepetiçãoFor.java
@@ -0,0 +1,12 @@
+public class estruturaDeRepetiçãoFor {
+    public static void main(String[] args) throws Exception {
+        
+
+
+        for (int numero = 10; numero >= 0; numero--){
+            System.out.println("Numero atual e:" + numero);
+        }
+
+
+    }
+}
